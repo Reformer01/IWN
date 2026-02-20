6f983b76cccf2a1fcf10bd9241b2986358c76e64
Reformer01 <reformerejembi22@gmail.com>
2026-02-20 11:23:00 +0100

    Restructured website to flat structure and cleaned up development folders

--- iworld_redesign_preview/Assets/forms/config.php
+++
@@ -1,5 +0,0 @@
-<?php
-define('TITLE', "i-world Networks");
-define('MAIL_USERNAME', "no_reply@iwn.ng");
-define('MAIL_PASSWORD', "dIPq#nxv}J?v");
-?>
--- iworld_redesign_preview/Assets/js/preloader.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    window.addEventListener("load", function() {
-        var preloader = document.querySelector(".preloader-orbit-loading");
-        preloader.style.display = "none";
-    });
-});
-
-
